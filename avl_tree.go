b1d5cffcf00e3fa8a6ac0240158e0e968b739325
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2021-12-02 00:24:51 +0900

    avl tree has finished

--- avl_tree.go
+++ avl_tree.go
@@ -13,17 +13,42 @@ func NewAvl () *Avl {
 
 type Node struct {
 	value int
-	balance int
+	height int
 	right *Node
 	left *Node
 }
 
 func NewNode(value int) *Node {
-	return &Node{value: value}
+	return &Node{
+		value: value,
+		height: 1,
+	}
 }
 
 func (n *Node) Value() int {
 	return n.value
+}
+
+func (n *Node) Height() int {
+	if n != nil {
+		return n.height
+	}
+	return 0
+}
+
+func (n *Node) getBalance() int {
+	return n.right.Height() - n.left.Height()
+}
+
+func (n *Node) updateHeight() int {
+	left := n.left.Height() + 1
+	right := n.right.Height() + 1
+	if left > right {
+		n.height = left
+	} else {
+		n.height = right
+	}
+	return n.height
 }
 
 func (t *Avl) Find(value int) (n *Node, route []*Node) {
@@ -41,15 +66,15 @@ func (t *Avl) Find(value int) (n *Node, route []*Node) {
 	return nil, route
 }
 
-func (t *Avl) Add(value int) *Node {
+func (t *Avl) Add(value int) bool {
 	if t.root == nil {
 		t.root = NewNode(value)
-		return t.root
+		return true
 	}
 	_, route := t.Find(value)
 	parent := route[len(route)-1]
 	if parent.value == value {
-		return parent
+		return false
 	}
 	nn := NewNode(value)
 	if value > parent.value {
@@ -57,7 +82,8 @@ func (t *Avl) Add(value int) *Node {
 	} else {
 		parent.left = nn
 	}
-	return parent
+	t.balance(route, true)
+	return true
 }
 
 func (t *Avl) Remove(value int) bool {
@@ -69,15 +95,22 @@ func (t *Avl) Remove(value int) bool {
 		return false
 	}
 	parent := t.getParentFromRoute(route)
-	if n.left != nil && n.right != nil {
-		t.removeNodeHasBothChildren(n)
-	} else if n.left != nil {
-		t.removeNodeHasLeft(n, parent)
-	} else if n.right != nil {
-		t.removeNodeHasRight(n, parent)
-	} else {
+	if n.left == nil && n.right == nil {
 		t.removeNodeHasNoChild(n, parent)
+		route = route[:len(route)-1]
+	} else if n.right == nil {
+		t.removeNodeHasChild(n, parent, n.left)
+		route = route[:len(route)-1]
+	} else if n.left == nil {
+		t.removeNodeHasChild(n, parent, n.right)
+		route = route[:len(route)-1]
+	} else {
+		additionalRoute := t.removeNodeHasChildren(n)
+		for i:=0; i<len(additionalRoute)-1; i++ {
+			route = append(route, additionalRoute[i])
+		}
 	}
+	t.balance(route, false)
 	return true
 }
 
@@ -88,7 +121,7 @@ func (*Avl) getParentFromRoute(route []*Node) *Node {
 	return nil
 }
 
-func (t *Avl) removeNodeHasBothChildren(n *Node) {
+func (t *Avl) removeNodeHasChildren(n *Node) []*Node {
 	leftMax, route := t.Max(n.left)
 	leftMaxParent := n
 	if leftMax != n.left {
@@ -98,18 +131,14 @@ func (t *Avl) removeNodeHasBothChildren(n *Node) {
 	if leftMax.left == nil {
 		t.removeNodeHasNoChild(leftMax, leftMaxParent)
 	} else {
-		t.removeNodeHasLeft(leftMax, leftMaxParent)
+		t.removeNodeHasChild(leftMax, leftMaxParent, leftMax.left)
 	}
+	return route
 }
 
-func (t *Avl) removeNodeHasLeft(n, parent *Node) {
-	t.replaceNode(n, parent, n.left)
-	n.left = nil
-}
-
-func (t *Avl) removeNodeHasRight(n, parent *Node) {
-	t.replaceNode(n, parent, n.right)
-	n.right = nil
+func (t *Avl) removeNodeHasChild(n, parent, child *Node) {
+	t.replaceNode(n, parent, child)
+	child = nil
 }
 
 func (t *Avl) removeNodeHasNoChild(n, parent *Node) {
@@ -178,12 +207,44 @@ func (t *Avl) echo(n *Node, space string) {
 	}
 }
 
-func (t *Avl) balance(n, parent, pivot *Node) {
+func (t *Avl) balance(route []*Node, isAdd bool) {
+	for i:=len(route)-1; i>=0; i-- {
+		n := route[i]
+		var parent *Node
+		if i != 0 {
+			parent = route[i-1]
+		}
+		n.updateHeight()
+		switch n.getBalance() {
+		case -1:
+			if !isAdd { return }
+		case -2:
+			if n.left.getBalance() > 0 {
+				t.rotateLR(n, parent, n.left)
+			} else {
+				t.rotateR(n, parent, n.left)
+			}
+			if isAdd { return }
+		case 0:
+			if isAdd { return }
+		case 1:
+			if !isAdd { return }
+		case 2:
+			if n.right.getBalance() < 0 {
+				t.rotateRL(n, parent, n.right)
+			} else {
+				t.rotateL(n, parent, n.right)
+			}
+			if isAdd { return }
+		}
+	}
 }
 
-func (t *Avl) rotateL(n, parent, pivot *Node) {
-	n.right = pivot.left
-	pivot.left = n
+func (t *Avl) setPivotAsParentsChild(n, parent, pivot *Node) {
+	if parent == nil {
+		t.root = pivot
+		return
+	}
 	if parent.right == n {
 		parent.right = pivot
 	} else {
@@ -191,13 +252,30 @@ func (t *Avl) rotateL(n, parent, pivot *Node) {
 	}
 }
 
+func (t *Avl) rotateL(n, parent, pivot *Node) {
+	n.right = pivot.left
+	pivot.left = n
+	n.updateHeight()
+	pivot.updateHeight()
+	t.setPivotAsParentsChild(n, parent, pivot)
+}
+
 func (t *Avl) rotateR(n, parent, pivot *Node) {
+	n.left = pivot.right
+	pivot.right = n
+	n.updateHeight()
+	pivot.updateHeight()
+	t.setPivotAsParentsChild(n, parent, pivot)
 }
 
 func (t *Avl) rotateLR(n, parent, pivot *Node) {
+	t.rotateL(pivot, n, pivot.right)
+	t.rotateR(n, parent, n.left)
 }
 
 func (t *Avl) rotateRL(n, parent, pivot *Node) {
+	t.rotateR(pivot, n, pivot.left)
+	t.rotateL(n, parent, n.right)
 }
 
 func main() {

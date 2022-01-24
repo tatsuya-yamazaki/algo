7208e1a99044dd65b597f117c648c9e7ad996852
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-01-24 20:11:22 +0900

    queue is refined, deque is added

---
+++ deque.go
@@ -0,0 +1,131 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	var d Deque
+
+	d.AddLeft(1)
+	d.AddLeft(2)
+	d.AddLeft(3)
+	d.AddLeft(4)
+	d.AddLeft(5)
+	d.AddLeft(6)
+	d.AddLeft(7)
+	d.AddLeft(8)
+	for d.Next() {
+		fmt.Println(d.PopRight())
+	}
+
+	d.AddLeft(1)
+	d.AddLeft(2)
+	d.AddLeft(3)
+	d.AddLeft(4)
+	d.AddLeft(5)
+	d.AddLeft(6)
+	d.AddLeft(7)
+	d.AddLeft(8)
+	for d.Next() {
+		fmt.Println(d.PopLeft())
+	}
+
+	d.AddRight(1)
+	d.AddRight(2)
+	d.AddRight(3)
+	d.AddRight(4)
+	d.AddRight(5)
+	d.AddRight(6)
+	d.AddRight(7)
+	d.AddRight(8)
+	for d.Next() {
+		fmt.Println(d.PopRight())
+	}
+
+	d.AddRight(1)
+	d.AddRight(2)
+	d.AddRight(3)
+	d.AddRight(4)
+	d.AddRight(5)
+	d.AddRight(6)
+	d.AddRight(7)
+	d.AddRight(8)
+	for d.Next() {
+		fmt.Println(d.PopLeft())
+	}
+
+	d.AddLeft(1)
+	d.AddRight(2)
+	d.AddLeft(3)
+	d.AddRight(4)
+	d.AddLeft(5)
+	d.AddRight(6)
+	d.AddLeft(7)
+	d.AddRight(8)
+	for d.Next() {
+		fmt.Println(d.PopLeft())
+	}
+
+}
+
+type Deque struct {
+	begin *LinkedList
+	end *LinkedList
+}
+
+func (d *Deque) Next() bool {
+	if d.begin == nil {
+		return false
+	}
+	return true
+}
+
+func (d *Deque) AddLeft(value int) {
+	ll := &LinkedList{nil, d.begin, value}
+	if d.begin == nil {
+		d.end = ll
+	} else {
+		d.begin.prev = ll
+	}
+	d.begin = ll
+}
+
+func (d *Deque) AddRight(value int) {
+	ll := &LinkedList{d.end, nil, value}
+	if d.end == nil {
+		d.begin = ll
+	} else {
+		d.end.next = ll
+	}
+	d.end = ll
+}
+
+func (d *Deque) PopLeft() int {
+	value := d.begin.value
+	if d.begin == d.end {
+		d.begin = nil
+		d.end = nil
+	} else {
+		d.begin.next.prev = nil
+		d.begin = d.begin.next
+	}
+	return value
+}
+
+func (d *Deque) PopRight() int {
+	value := d.end.value
+	if d.begin == d.end {
+		d.begin = nil
+		d.end = nil
+	} else {
+		d.end.prev.next = nil
+		d.end = d.end.prev
+	}
+	return value
+}
+
+type LinkedList struct {
+        prev, next *LinkedList
+	value int
+}
--- queue.go
+++ queue.go
@@ -7,17 +7,20 @@ import(
 func main() {
 	var q Queue
 
-	q.add(1)
-	q.add(2)
-	q.add(3)
-	q.add(4)
-	q.add(5)
-	q.add(6)
-	q.add(7)
-	q.add(8)
+	q.Add(1)
+	q.Add(2)
+	q.Add(3)
+	q.Add(4)
+	for q.Next() {
+		fmt.Println(q.Pop())
+	}
 
-	for q.next() {
-		fmt.Println(q.pop())
+	q.Add(5)
+	q.Add(6)
+	q.Add(7)
+	q.Add(8)
+	for q.Next() {
+		fmt.Println(q.Pop())
 	}
 }
 
@@ -26,30 +29,32 @@ type Queue struct {
 	end *LinkedList
 }
 
-func (q *Queue) next() bool {
+func (q *Queue) Next() bool {
 	if q.begin == nil {
 		return false
 	}
 	return true
 }
 
-func (q *Queue) add(value int) {
+func (q *Queue) Add(value int) {
 	ll := &LinkedList{q.end, nil, value}
-	if q.begin == nil {
+	if q.end == nil {
 		q.begin = ll
-	}
-	if q.end != nil {
+	} else {
 		q.end.next = ll
 	}
 	q.end = ll
 }
 
-func (q *Queue) pop() int {
+func (q *Queue) Pop() int {
 	value := q.begin.value
 	if q.begin == q.end {
+		q.begin = nil
 		q.end = nil
+	} else {
+		q.begin.next.prev = nil
+		q.begin = q.begin.next
 	}
-	q.begin = q.begin.next
 	return value
 }
 

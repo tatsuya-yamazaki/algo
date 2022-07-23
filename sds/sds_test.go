cd9d24a35e7f92fbcaa180e661ad59a8aa4c3145
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-07-24 00:32:32 +0900

    add benchmark Select Select0

--- sds/sds_test.go
+++ sds/sds_test.go
@@ -3,6 +3,7 @@
 package sds
 
 import (
+	"math/rand"
 	"testing"
 )
 
@@ -76,5 +77,37 @@ func TestSelect0(t *testing.T) {
 		if a := s.Select0(i); i != a {
 			t.Errorf("%v != %v", i, a)
 		}
+	}
+}
+
+func BenchmarkSelect(b *testing.B) {
+	s := NewSuccinctDictionary(200000)
+	c := 0
+	for i:=0; i<200000; i++ {
+		if rand.Intn(1) > 0 {
+			c++
+			s.Set(i, true)
+		}
+	}
+	s.Build()
+	b.ResetTimer()
+	for i:=0; i<b.N; i++ {
+		s.Select(rand.Intn(c+1))
+	}
+}
+
+func BenchmarkSelect0(b *testing.B) {
+	s := NewSuccinctDictionary(200000)
+	c := 200000
+	for i:=0; i<200000; i++ {
+		if rand.Intn(1) > 0 {
+			c--
+			s.Set(i, true)
+		}
+	}
+	s.Build()
+	b.ResetTimer()
+	for i:=0; i<b.N; i++ {
+		s.Select0(rand.Intn(c+1))
 	}
 }
